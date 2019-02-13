fceb99dd9643d1be44ea3fe30ad1d443b6477c8b
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-02-13 13:09:46 -0700

    Rewrite of notification Content Storyboard with progressBar

--- Content/NotificationViewController.swift
+++ Content/NotificationViewController.swift
@@ -13,20 +13,129 @@ import UserNotificationsUI
 class NotificationViewController: UIViewController, UNNotificationContentExtension {
 
     //MARK: - Outlets
-    @IBOutlet weak var streakNameLabel: UILabel!
-    @IBOutlet weak var streakNumberLabel: UILabel!
-    @IBOutlet weak var streakTextLabel: UILabel!
+    let smallSquareView = UIView()
+    
+    @IBOutlet weak var progressBarView: ViewWithProgressBar!
+    
+    let streakNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Start new Streak"
+        label.font = UIFont.preferredFont(forTextStyle: UIFont.TextStyle.title1)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.clipsToBounds = false
+        label.textAlignment = .center
+        label.adjustsFontSizeToFitWidth = true
+        label.baselineAdjustment = .alignCenters
+        return label
+    }()
+    
+    let streakNumberLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont.monospacedDigitSystemFont(ofSize: 100, weight: UIFont.Weight.light)
+        label.text = "0"
+        label.clipsToBounds = true
+
+        label.adjustsFontSizeToFitWidth = true
+        label.baselineAdjustment = .alignCenters
+        return label
+    }()
+    let roundDaysbutton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Day", for: .normal)
+        button.titleLabel?.font = UIFont.preferredFont(forTextStyle: UIFont.TextStyle.title1)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.adjustsFontSizeToFitWidth = true
+        button.clipsToBounds = false
+        button.titleLabel?.baselineAdjustment = .alignCenters
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .white
+        button.layer.borderWidth = 3
+        button.layer.cornerRadius = button.frame.size.width / 2.0
+        button.layer.borderColor = UIColor.lightGray.withAlphaComponent(0.5).cgColor
+        return button
+    }()
+    
+    let streakBodyLabel: UILabel = {
+        let label = UILabel()
+        label.text = "BodyText here"
+        label.font = UIFont.preferredFont(forTextStyle: UIFont.TextStyle.title1)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.clipsToBounds = false
+        label.textAlignment = .center
+        label.adjustsFontSizeToFitWidth = true
+        label.baselineAdjustment = .alignCenters
+        return label
+    }()
+    
+    
+    //MARK: - Properties
+    var progress:Float = 1.0
     
     //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any required interface initialization here.
     }
     
     func didReceive(_ notification: UNNotification) {
-        self.streakTextLabel?.text = notification.request.content.body
-        self.streakNameLabel?.text = notification.request.content.title
-        self.streakNumberLabel?.text = notification.request.content.userInfo["count"] as! String
+//        self.streakTextLabel?.text = notification.request.content.body
+        self.streakNameLabel.text = notification.request.content.title
+        self.streakNumberLabel.text = notification.request.content.userInfo["count"] as? String
     }
+    
+    
+    //MARK: - Add ProgressTriangle
+    func updateProgressTriangle(){
+        view.addSubview(smallSquareView)
+        smallSquareView.translatesAutoresizingMaskIntoConstraints = false
+        smallSquareView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5).isActive = true
+        smallSquareView.heightAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5).isActive = true
+        smallSquareView.topAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        smallSquareView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+    }
+    
+    //MARK: - Private Functions
+    func updateUI() {
+        let margins = view.safeAreaLayoutGuide
+
+        updateProgressTriangle()
+        
+        progressBarView.progress = progress
+        progressBarView.setNeedsDisplay()
+        
+        let notificationStack = UIStackView(arrangedSubviews: [streakNameLabel,streakNumberLabel,streakBodyLabel])
+        notificationStack.alignment = .fill
+        notificationStack.distribution = .fill
+        notificationStack.spacing = 50
+        notificationStack.axis = .vertical
+        
+        view.addSubview(notificationStack)
+        notificationStack.sizeToFit()
+        
+//        view.addSubview(streakNameLabel)
+//        streakNameLabel.topAnchor.constraint(equalTo: margins.topAnchor).isActive = true
+//        streakNameLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+//        streakNameLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+//        streakNameLabel.heightAnchor.constraint(equalTo: view.heightAnchor , multiplier: 0.20).isActive = true
+//
+//        view.addSubview(streakNumberLabel)
+//        streakNumberLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+//        streakNumberLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        view.addSubview(roundDaysbutton)
+        roundDaysbutton.bottomAnchor.constraint(equalTo: smallSquareView.bottomAnchor, constant: -5).isActive = true
+        roundDaysbutton.rightAnchor.constraint(equalTo: smallSquareView.rightAnchor, constant: -5).isActive = true
+        roundDaysbutton.heightAnchor.constraint(equalTo: smallSquareView.heightAnchor, multiplier: 0.53).isActive = true
+        roundDaysbutton.widthAnchor.constraint(equalTo: smallSquareView.widthAnchor, multiplier: 0.53).isActive = true
+        roundDaysbutton.layer.cornerRadius = 0.25 * view.bounds.size.width * 0.53
+        
+//        view.addSubview(streakBodyLabel)
+//        streakBodyLabel.bottomAnchor.constraint(equalTo: margins.bottomAnchor).isActive = true
+//        streakBodyLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20).isActive = true
+//        streakBodyLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20).isActive = true
+//        streakBodyLabel.heightAnchor.constraint(equalTo: view.heightAnchor , multiplier: 0.20).isActive = true
+        
+    }
+
 
 }
---
+++ Content/ViewWithProgressBar.swift
@@ -0,0 +1,41 @@
+//
+//  ViewWithProgressBar.swift
+//  Content
+//
+//  Created by Dominic Lanzillotta on 2/13/19.
+//  Copyright © 2019 Dominic Lanzillotta. All rights reserved.
+//
+
+import UIKit
+
+class ViewWithProgressBar: UIView {
+    
+    var progress:Float = 1.0
+
+
+    override func draw(_ rect: CGRect) {
+        let smallSquare = CGRect(x: self.bounds.midX - 2, y: self.bounds.midY, width: self.bounds.width * 0.5 , height: self.bounds.width * 0.5)
+        
+        let trianglePath = UIBezierPath()
+        trianglePath.move(to: CGPoint(x: smallSquare.minX , y: smallSquare.maxY))
+        trianglePath.addLine(to: CGPoint(x: smallSquare.maxX, y: smallSquare.maxY))
+        trianglePath.addLine(to: CGPoint(x: smallSquare.maxX, y: smallSquare.minY))
+        trianglePath.addLine(to: CGPoint(x: smallSquare.minX , y: smallSquare.maxY))
+        UIColor.lightGray.withAlphaComponent(0.5).setFill()
+        trianglePath.lineCapStyle = .round
+        trianglePath.close()
+        trianglePath.fill()
+        
+        let valX = ((smallSquare.width / 100) * CGFloat(progress * 100)) - 6
+        let valY = (smallSquare.width - valX)
+        
+        let progressBezier = UIBezierPath()
+        progressBezier.move(to: CGPoint(x: smallSquare.minX + 3, y: smallSquare.maxY - 3))
+        progressBezier.addLine(to: CGPoint(x: smallSquare.minX + 3 + min(valX,smallSquare.width - 6), y: smallSquare.minY - 3 + max(valY,0 + 6)))
+        UIColor.lushGreenColor.setStroke()
+        progressBezier.lineWidth = 7
+        progressBezier.lineCapStyle = .round
+        progressBezier.stroke()
+    }
+
+}
