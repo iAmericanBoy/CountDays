baee92df25cb237ffda8429d1f335e2fdae89fd5
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-02-25 13:04:52 -0700

    Fixed bug that caused crash on savescreen with the attributed string

--- CountDays/SaveScreenTableView/SaveScreenViewController.swift
+++ CountDays/SaveScreenTableView/SaveScreenViewController.swift
@@ -247,7 +247,7 @@ class SaveScreenViewController: UIViewController, UIPopoverPresentationControlle
         let url = URL(string: "https://www.twitter.com/iAmericanBoy")!
 
         // Set the 'click here' substring to be the link
-        attributedString.setAttributes([.link: url, .font: UIFont.systemFont(ofSize: UIFont.buttonFontSize)], range: NSUnionRange(NSRange(myName)!, NSRange(attributedString.string)!))
+        attributedString.setAttributes([.link: url, .font: UIFont.systemFont(ofSize: UIFont.buttonFontSize)], range: NSRange(attributedString.string.range(of: myName)!, in: attributedString.string))
 
         textField.attributedText = attributedString
         
